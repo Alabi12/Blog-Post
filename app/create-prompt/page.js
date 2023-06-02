84101a7f7b31dbb6560b3754486f5bc18164c68c
Alabi12 <wolo22001@gmail.com>
2023-06-02 01:20:52 +0000

    Add form post data to the database, populate to ui

--- app/create-prompt/page.js
+++ app/create-prompt/page.js
@@ -1,6 +1,49 @@
-const page = () => {
+"use client";
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Form from "@components/Form";
+
+const CreatePrompt = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <div>page</div>
-  )
-}
-export default page
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitted}
+      handleSubmit={createPrompt}
+    />
+  );
+};
+export default CreatePrompt;
